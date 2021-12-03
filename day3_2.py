7c35f5ea427ab1b7e609727f58cf670fc8abd68b
kennedn <kennedn@msn.com>
2021-12-03 14:09:35 +0000

    functionised reused code snippet

--- day3_2.py
+++ day3_2.py
@@ -6,17 +6,20 @@ lines = advent_opener('day3.txt')
 length = len(lines[0]) - 1
 vals = [int(b, 2) for b in lines]
 
+def first_bit(val, shift):
+    return (val >> shift) & 0x01
+
 def eliminator(array, bit_length, one_most_common):
     elim_vals = array
-    for shift in range(bit_length - 1, -1, -1):
-        one_bits = [v for v in elim_vals if (v >> shift) & 0x01 == 1]
-        zero_bits = [v for v in elim_vals if (v >> shift) & 0x01 == 0]
+    for shift in reversed(range(bit_length)):
+        one_bits = [v for v in elim_vals if first_bit(v, shift) == 1]
+        zero_bits = [v for v in elim_vals if first_bit(v, shift) == 0]
         most_common = None
         if len(one_bits) >= len(zero_bits):
             most_common = 1 if one_most_common else 0
         else:
             most_common = 0 if one_most_common else 1
-        elim_vals = [v for v in elim_vals if (v >> shift) & 0x01 != most_common]
+        elim_vals = [v for v in elim_vals if first_bit(v, shift) != most_common]
         if(len(elim_vals) == 1):
             return elim_vals[0]
 
