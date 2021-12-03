896d5b591bd1ba4f89895040d556069c08c715da
kennedn <kennedn@msn.com>
2021-12-03 09:50:04 +0000

    Day3_2 cleanup

--- day3_2.py
+++ day3_2.py
@@ -5,35 +5,21 @@ lines = advent_opener('day3.txt')
 
 length = len(lines[0]) - 1
 vals = [int(b, 2) for b in lines]
-epsilon_vals = vals.copy()
-gamma_vals = vals.copy()
-gamma = 0
-epsilon = 0
-for shift in range(length - 1, -1, -1):
-    one_bits = [v for v in gamma_vals if (v >> shift) & 0x01 == 1]
-    zero_bits = [v for v in gamma_vals if (v >> shift) & 0x01 == 0]
-    most_common = None
-    if len(one_bits) >= len(zero_bits):
-        most_common = 1
-    else:
-        most_common = 0
-    gamma_vals = [v for i, v in enumerate(gamma_vals) if (v >> shift) & 0x01 == most_common]
-    if(len(gamma_vals) == 1):
-        gamma = gamma_vals[0]
-        break
 
-print(gamma)
-for shift in range(length - 1, -1, -1):
-    one_bits = [v for v in epsilon_vals if (v >> shift) & 0x01 == 1]
-    zero_bits = [v for v in epsilon_vals if (v >> shift) & 0x01 == 0]
-    most_common = None
-    if len(one_bits) >= len(zero_bits):
-        most_common = 1
-    else:
-        most_common = 0
-    epsilon_vals = [v for i, v in enumerate(epsilon_vals) if (v >> shift) & 0x01 != most_common]
-    if(len(epsilon_vals) == 1):
-        epsilon = epsilon_vals[0]
-        break
+def eliminator(array, bit_length, one_most_common):
+    elim_vals = array
+    for shift in range(bit_length - 1, -1, -1):
+        one_bits = [v for v in elim_vals if (v >> shift) & 0x01 == 1]
+        zero_bits = [v for v in elim_vals if (v >> shift) & 0x01 == 0]
+        most_common = None
+        if len(one_bits) >= len(zero_bits):
+            most_common = 1 if one_most_common else 0
+        else:
+            most_common = 0 if one_most_common else 1
+        elim_vals = [v for i, v in enumerate(elim_vals) if (v >> shift) & 0x01 != most_common]
+        if(len(elim_vals) == 1):
+            return elim_vals[0]
 
+gamma = eliminator(vals, length, True)
+epsilon = eliminator(vals, length, False)
 print(epsilon*gamma)
