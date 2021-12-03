b901329e103663d53433794e34a31023ad59d27a
kennedn <kennedn@msn.com>
2021-12-03 14:29:20 +0000

    Swapped most_common to make sense

--- day3_2.py
+++ day3_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from file_handler import advent_opener
 
-lines = advent_opener('day3.txt')
+lines = advent_opener('day3_example.txt')
 
 length = len(lines[0]) - 1
 vals = [int(b, 2) for b in lines]
@@ -14,7 +14,7 @@ def eliminator(array, bit_length, one_most_common):
     for shift in reversed(range(bit_length)):
         one_bits = len([v for v in elim_vals if first_bit(v, shift) == 1])
         zero_bits = len([v for v in elim_vals if first_bit(v, shift) == 0])
-        most_common = one_bits - zero_bits < 0 if one_most_common else one_bits - zero_bits >= 0
+        most_common =  one_bits >= zero_bits if one_most_common else one_bits < zero_bits
         elim_vals = [v for v in elim_vals if first_bit(v, shift) == most_common]
         if(len(elim_vals) == 1):
             return elim_vals[0]
