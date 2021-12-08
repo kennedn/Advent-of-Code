9cb885b83f9d6a4ca455908e3618cf5786d8e9c0
kennedn <kennedn@msn.com>
2021-12-08 20:53:49 +0000

    Code cleanup

--- day8_2.py
+++ day8_2.py
@@ -5,7 +5,7 @@ import itertools as it
 digits = {
     'abcefg': 0,
     'cf': 1,
-    'acdeg': 3,
+    'acdeg': 2,
     'acdfg': 3,
     'bcdf': 4,
     'abdfg': 5,
@@ -42,46 +42,59 @@ wire_map = {
     'g': 'abcdefg'
 }
 
-def get_lengths(digit_array):
-    return [length_map[digit] for digit in digit_array]
+def get_unique_lengths(length_map):
+    len_list = list(length_map.values())
+    return [l for l in len_list if len_list.count(l) == 1]
 
 def reverse_digit_map(digit_map):
     return [c for c in 'abcdefg' if c not in digit_map]
 
-def derive_numbers(wi_map, scrambled_numbers):
-    keys, values = zip(*wi_map.items())
-    permutations_dicts = [dict(zip(keys, v)) for v in it.product(*values)]
-    for m in permutations_dicts:
-        m_r = {v:k for k,v in m.items()}
-        if len(m_r.keys()) < 7:
-            continue
+def derive_numbers(wire_map, scrambled_numbers):
+    keys = list(wire_map.keys())
+    values = list(wire_map.values())
+    # Get all possible permutations of map from partially resolved wire_map
+    permutations = [dict(zip(keys, v)) for v in it.product(*values)]
+    # invert maps so they now become lookup tables for scrambled numbers
+    permutations = [{v:k for k,v in dict.items()} for dict in permutations]
+    # Filter out duds (those that had duplicate values before inversion) 
+    permutations = [dict for dict in permutations if len(dict.keys()) == 7]
+    for m in permutations:
         ret_nums = []
         for num in scrambled_numbers:
-            number = ''.join(sorted([m_r[c] for c in num]))
-            if number in digits:
-                ret_nums.append(digits[number])
+            decoded_number = ''.join(sorted([m[c] for c in num]))
+            if decoded_number in digits:
+                ret_nums.append(digits[decoded_number])
                 continue
         if len(ret_nums) == 4:
             return int(''.join([str(n) for n in ret_nums]))
+    raise Exception("Something went wrong")
 
 # raw = advent_opener("day_8_example.txt")
 # raw = advent_opener("day_8_example_short.txt")
 raw = advent_downloader(8)
-segments = [l.split('|') for l in raw]
-for i, seg in enumerate(segments):
-    for j, s in enumerate(seg):
-        segments[i][j] = s.replace("|", "").replace("\n", "").strip().split(" ")
+segment_list = [l.split('|') for l in raw]
+for i, segment in enumerate(segment_list):
+    for j, s in enumerate(segment):
+        segment_list[i][j] = s.replace("\n", "").strip().split(" ")
 
+unique_lengths = get_unique_lengths(length_map)
 solve_sum = 0
-for seg in segments:
-    wmap = dict(wire_map)
-    for digit in seg[0]:
+for segment, output in segment_list:
+    _wire_map = dict(wire_map)
+    for digit in segment:
         digit_length = len(digit)
-        if digit_length in get_lengths([1, 4, 7, 8]):
-            real_digit_map = digits_invert[length_map_invert[digit_length]]
-            for c in real_digit_map:
-                wmap[c] = ''.join([s for s in wmap[c] if s in digit])
-            for c in reverse_digit_map(real_digit_map):
-                wmap[c] = ''.join([s for s in wmap[c] if s not in digit])
-    solve_sum += derive_numbers(wmap, seg[1])
+        # Use known digits (with unique length) to partially solve wire_map
+        if digit_length in unique_lengths:
+            unscrambled_digit_map = digits_invert[length_map_invert[digit_length]]
+            # For each segment in unscrambled digit map, 
+            # filter out segments that DO NOT occur in the scrambled digit
+            for c in unscrambled_digit_map:
+                _wire_map[c] = ''.join([s for s in _wire_map[c] if s in digit])
+
+            # For each segment NOT in unscrambled digit map, 
+            # filter out segments that DO occur in the scrambled digit 
+            for c in reverse_digit_map(unscrambled_digit_map):
+                _wire_map[c] = ''.join([s for s in _wire_map[c] if s not in digit])
+    solve_sum += derive_numbers(_wire_map, output)
 print(solve_sum)
+
