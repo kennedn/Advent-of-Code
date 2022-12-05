ab59ddfb8262a849311741e10054b6037bd854e3
kennedn <kennedn@msn.com>
2022-12-05 13:54:35 +0000

    Added seperator detection

--- 2022/day5/2.py
+++ 2022/day5/2.py
@@ -8,10 +8,12 @@ import string
 
 rows = [r.strip("\n") for r in advent_downloader(5)]
 
-crates = [[f[1] for f in re.findall(r'\[[A-Z]\]|\s\s\s\s', r)] for r in rows[:8]]
+separator = next(i for i,r in enumerate(rows) if r == "")
+
+crates = [[f[1] for f in re.findall(r'\[[A-Z]\]|\s\s\s\s', r)] for r in rows[:separator-1]]
 crates = [[c for c in r if c in string.ascii_uppercase] for r in list(map(list,zip(*crates[::-1])))]
 
-moves = [re.findall(r'\d{1,2}', r) for r in rows[10::]]
+moves = [re.findall(r'\d{1,2}', r) for r in rows[separator+1::]]
 moves = [[int(m[0]), int(m[1])-1, int(m[2])-1] for m in moves]
 
 def print_top_crates():
