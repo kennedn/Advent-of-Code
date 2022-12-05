eaa10d5c5d48fbdc881c57eda65e8f8a2c9f515d
kennedn <kennedn@msn.com>
2022-12-05 11:47:58 +0000

    Squid from 2021 and day 5

---
+++ 2021/day11/1.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("..") 
+from file_handler import advent_downloader, advent_opener
+
+import re
+
+def reset_flashmap():
+    global flash_map
+    flash_map = []
+    for y in range(len(squids)):
+        flash_map.append([False] * len(squids[y]))
+
+squids = [re.findall('\d', r) for r in advent_downloader(11)]
+# squids = [re.findall('\d', r) for r in advent_opener('day_11_example.txt')]
+squids = [list(map(int,s)) for s in squids]
+flash_map = []
+reset_flashmap()
+
+    
+def flash(x, y, init=False):
+    if not init: squids[y][x] += 1
+
+    if (squids[y][x] <= 9 or flash_map[y][x]):
+        # squids[y][x] += 1
+        return
+
+    print(f'{x=}, {y=}')
+    flash_map[y][x] = True
+
+    if x-1 >= 0:
+        flash(x-1,y)
+    if x+1 <= len(squids[y]) - 1:
+        flash(x+1, y)
+
+    if y-1 >= 0:
+        flash(x,y-1)
+        if x-1 >= 0:
+            flash(x-1,y-1)
+        if x+1 <= len(squids[y]) - 1:
+            flash(x+1, y-1)
+
+    if y+1 <= len(squids) - 1:
+        flash(x,y+1)
+        if x-1 >= 0:
+            flash(x-1,y+1)
+        if x+1 <= len(squids[y]) - 1:
+            flash(x+1,y+1)
+
+def print_squids():
+    for y in range(len(squids)):
+        print(squids[y])
+
+def step():
+    reset_flashmap()
+
+    for y in range(len(squids)):
+        for x in range(len(squids[y])):
+            squids[y][x] += 1
+                
+    for y in range(len(squids)):
+        for x in range(len(squids[y])):
+            flash(x, y, True)
+
+    for y in range(len(squids)):
+        for x in range(len(squids[y])):
+            if squids[y][x] > 9:
+                squids[y][x] = 0
+
+    return sum(row.count(True) for row in flash_map)
+
+    
+flashes = 0
+for i in range(100):
+    flashes += step()
+
+print(flashes)
+
+
+        
+
+
---
+++ 2022/day5/1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("..") 
+from file_handler import advent_downloader, advent_opener
+
+import re
+import string
+
+rows = [r.strip("\n") for r in advent_downloader(5)]
+
+crates = [[f[1] for f in re.findall(r'\[[A-Z]\]|\s\s\s\s', r)] for r in rows[:8]]
+crates = [[c for c in r if c in string.ascii_uppercase] for r in list(map(list,zip(*crates[::-1])))]
+
+moves = [re.findall(r'\d{1,2}', r) for r in rows[10::]]
+moves = [[int(m[0]), int(m[1])-1, int(m[2])-1] for m in moves]
+
+def print_top_crates():
+  s = ""
+  for c in crates:
+    if len(c) > 0:
+      s += c[-1]
+  print(s)
+
+def move_crates(qnty, src, dest):
+  for i in range(qnty):
+    crates[dest].append(crates[src].pop())
+
+for i,m in enumerate(moves):
+  move_crates(m[0], m[1], m[2])
+
+print_top_crates()
