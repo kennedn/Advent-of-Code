4b91b747229e7098fd59c6a3acb7222d75e53998
kennedn <kennedn@msn.com>
2022-12-09 15:52:57 +0000

    Cleaned up day 2

--- 2022/day9/2.py
+++ 2022/day9/2.py
@@ -5,9 +5,9 @@ from file_handler import advent_downloader, advent_opener
 
 import re
 
-# rows = [re.match(r'^([LRUD]) (\d+)$',r.strip("\n")) for r in advent_downloader(9)]
+rows = [re.match(r'^([LRUD]) (\d+)$',r.strip("\n")) for r in advent_downloader(9)]
 # rows = [re.match(r'^([LRUD]) (\d+)$',r.strip("\n")) for r in advent_opener('e.txt')]
-rows = [re.match(r'^([LRUD]) (\d+)$',r.strip("\n")) for r in advent_opener('e2.txt')]
+# rows = [re.match(r'^([LRUD]) (\d+)$',r.strip("\n")) for r in advent_opener('e2.txt')]
    
 moves = [[m[1],int(m[2])] for m in rows]
 
@@ -33,18 +33,11 @@ for j,d in enumerate(deltas):
       continue
     if abs(knots[i-1][0] - knots[i][0]) <= 1 and abs(knots[i-1][1] - knots[i][1]) <= 1 :
       continue
-    if knots[i-1][0] == knots[i][0] + 2 and knots[i-1][1] == knots[i][1]:
-      knots[i][0] += 1
-    elif knots[i-1][0] == knots[i][0] - 2 and knots[i-1][1] == knots[i][1]:
-      knots[i][0] -= 1
-    elif knots[i-1][1] == knots[i][1] + 2 and knots[i-1][0] == knots[i][0]:
-      knots[i][1] += 1
-    elif knots[i-1][1] == knots[i][1] - 2 and knots[i-1][0] == knots[i][0]:
-      knots[i][1] -= 1
-    else:
-        knots[i][0] += 1 if knots[i-1][0] > knots[i][0] else -1
-        knots[i][1] += 1 if knots[i-1][1] > knots[i][1] else -1
-
+    new_delta = [
+      1 if knots[i-1][0] > knots[i][0] else 0 if knots[i-1][0] == knots[i][0] else -1, 
+      1 if knots[i-1][1] > knots[i][1] else 0 if knots[i-1][1] == knots[i][1] else -1
+    ]
+    knots[i] = [knots[i][0] + new_delta[0], knots[i][1] + new_delta[1]]
   tail_visits.add(tuple(knots[-1]))
   print(d, knots)
 
